37e91cc18d43e1a973e2f586fc6a26f8777f2521
Hemanth Gokavarapu <5942165+hemanthgk10@users.noreply.github.com>
2026-07-24 15:10:23 -0700

    Update insecure_test.tf

--- example/insecure_test.tf
+++ example/insecure_test.tf
@@ -8,6 +8,7 @@ resource "aws_security_group" "cod7216_open_ssh" {
   description = "Test SG that allows SSH from anywhere"
 
 
+
   ingress {
     description = "SSH from anywhere"
     from_port   = 22
